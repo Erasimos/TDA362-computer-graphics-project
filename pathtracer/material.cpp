192931510605bf668ae75df9f16cfbffe47afae5
Erasimos <rasmus.tomasson@gmail.com>
2019-12-05 12:09:59 +0100

    unfinished importance sampling

--- pathtracer/material.cpp
+++ pathtracer/material.cpp
@@ -92,34 +92,54 @@ vec3 BlinnPhong::f(const vec3& wi, const vec3& wo, const vec3& n)
 
 vec3 BlinnPhong::sample_wi(vec3& wi, const vec3& wo, const vec3& n, float& p)
 {
+	/*
 	vec3 tangent = normalize(perpendicular(n));
 	vec3 bitangent = normalize(cross(tangent, n));
 	vec3 sample = cosineSampleHemisphere();
+	
+	
 	wi = normalize(sample.x * tangent + sample.y * bitangent + sample.z * n);
 	if(dot(wi, n) <= 0.0f)
 		p = 0.0f;
 	else
 		p = max(0.0f, dot(n, wi)) / M_PI;
 	return f(wi, wo, n);
+	*/
 
 	// TASK 6
-	/*
+	
+	vec3 tangent = normalize(perpendicular(n));
+	vec3 bitangent = normalize(cross(tangent, n));
+	float phi = 2.0f * M_PI * randf();
+	float cos_theta = pow(randf(), 1.0f / (shininess + 1));
+	float sin_theta = sqrt(max(0.0f, 1.0f - cos_theta * cos_theta));
+	vec3 wh = normalize(sin_theta * cos(phi) * tangent +
+		sin_theta * sin(phi) * bitangent +
+		cos_theta * n);
+	if (dot(wo, n) <= 0.0f) return vec3(0.0f);
+
+	
+	
+
 	if (randf() < 0.5) {
+
+		wi = reflect(wo, wh);
+		float pwh = (shininess + 1) * pow(dot(n, wh), shininess) / (2 * M_PI);
+		float p = pwh / (4 * (dot(wo, wh)));
+		
 		// Sample a direction based on the Microfacet brdf
-		(wi, pdf) < -<...>
-			pdf = pdf * 0.5;
+		p *= 0.5f;
 		return reflection_brdf(wi, wo, n);
 	}
 	else {
 		// Sample a direction for the underlying layer
-		(wi, brdf, pdf) < -refraction_layer->sample_wi(...);
-		pdf = pdf * 0.5;
+		
+		vec3 brdf = refraction_layer->sample_wi(wi,wo,n,p);
+		p *= 0.5f;
 		// We need to attenuate the refracted brdf with (1 - F)
-		F < -R0 + (1.0f - R0) * pow(1.0f - abs(dot(wh, wi)), 5.0f);
+		float F = -R0 + (1.0f - R0) * pow(1.0f - abs(dot(wh, wi)), 5.0f);
 		return (1 - F) * brdf;
 	}
-	*/
-
 }
 
 ///////////////////////////////////////////////////////////////////////////
